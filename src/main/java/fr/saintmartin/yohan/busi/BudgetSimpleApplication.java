60bbb44bdfb9f7b734a83a404e5e6badbba0671d
Y79S84M <yostm.dev@gmail.com>
2023-10-09 21:05:55 +0200

    initial commit - project initialisation

---
+++ src/main/java/fr/saintmartin/yohan/busi/BudgetSimpleApplication.java
@@ -0,0 +1,13 @@
+package fr.saintmartin.yohan.busi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetSimpleApplication.class, args);
+	}
+
+}
